e124a6575bd6dece26b753a47e83441bf94f8253
YuHu <101623679+YuHaRee@users.noreply.github.com>
2024-09-27 20:59:03 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+const sections = document.querySelectorAll('section');
+const navItems = document.querySelectorAll('#sidebar li');
+
+window.addEventListener('scroll', () => {
+    let current = '';
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+
+        if (pageYOffset >= sectionTop - sectionHeight / 3) {
+            current = section.getAttribute('id');
+        }
+    });
+
+    navItems.forEach(item => {
+        item.classList.remove('active');
+        if (item.getAttribute('data-section') === current) {
+            item.classList.add('active');
+        }
+    });
+});
